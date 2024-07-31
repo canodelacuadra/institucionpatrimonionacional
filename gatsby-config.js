54b3e8175466cfac1cdd726bcf7d846be7bcc3fd
canodelacuadra <canodelacuadra@gmail.com>
2024-07-31 10:54:24 +0200

    datos basicos

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Institución Patrimonio Nacional`,
+    description: `Recogemos todos los aspectos relevantes de nuestra institucion`,
+    author: `@canodelacuadra`,
+    siteUrl: `https://github.io/canodelacuadra`,
   },
   plugins: [
     `gatsby-plugin-image`,
